07ad1495cad67d3cda3f5a3ab103b8acc2db1db0
Son Nguyen <Son415@gwu.edu>
2025-02-25 18:40:24 +0100

    Hacky fix for now to prevent segfault

--- split.py
+++ split.py
@@ -4,6 +4,7 @@ import subprocess
 
 INPUT = "test"  
 OUTPUT = "result"
+TEST = "test.archobj"
 binary = lief.parse(INPUT)
 
 def run_command(command):
@@ -11,10 +12,13 @@ def run_command(command):
     return subprocess.run(command, shell=True, text=True, capture_output=True).stdout
 
 def output_result():
-    run_command(f"readelf -S {INPUT} > {INPUT}.txt")
-    run_command(f"readelf -S {OUTPUT} > {OUTPUT}.txt")
+    run_command(f"readelf -S {INPUT} > {INPUT}_sections.txt")
+    run_command(f"readelf -S {OUTPUT} > {OUTPUT}_sections.txt")
     run_command(f"chmod +x {OUTPUT}")
 
+    run_command(f"./{INPUT} < {TEST} > {INPUT}.txt")
+    run_command(f"./{OUTPUT} < {TEST} > {OUTPUT}.txt")
+    run_command(f"diff {INPUT}.txt {OUTPUT}.txt > diff.txt")
 
 def get_section(name):
     # Find the section
@@ -29,9 +33,10 @@ def extract_symbols(og_section):
     subsection_addresses = []
     for symbol in binary.symbols:
         if og_section.virtual_address <= symbol.value < og_section.virtual_address + og_section.size:
+            print("Symbol: ", symbol.name, symbol.value)
             subsection_addresses.append((symbol.value, symbol.name))
         
-    # Sort functions by address
+    # Sort by address
     subsection_addresses.sort()
     return subsection_addresses
 
@@ -41,6 +46,10 @@ def create_sections(og_section, subsection_addresses):
 
     # Create sections for each function
     for i, (sub_addr, sub_name) in enumerate(subsection_addresses):
+        if sub_name == "__data_start" or sub_name == "__dso_handle":
+            continue
+ 
+        print(f"Creating section for {sub_name} at {sub_addr}")
         # Determine function size
         if i < len(subsection_addresses) - 1:
             next_sub_addr = subsection_addresses[i + 1][0]
@@ -58,22 +67,24 @@ def create_sections(og_section, subsection_addresses):
         new_section.virtual_address = sub_addr
         new_section.size = len(function_bytes)
         new_section.alignment = og_section.alignment
-        new_section.type = og_section.type
         new_section.flags = og_section.flags
+            
 
         # Add the new section
-        binary.add(new_section)
+        binary.add(new_section, loaded=True)  
 
     # Remove the original section
     binary.remove(og_section)
+
+data_section = get_section(".data")
+variable_addresses = extract_symbols(data_section)
+create_sections(data_section, variable_addresses)
 
 text_section = get_section(".text")
 subsection_addresses = extract_symbols(text_section)
 create_sections(text_section, subsection_addresses)
 
-# data_section = get_section(".data")
-# variable_addresses = extract_symbols(data_section)
-# create_sections(data_section, variable_addresses)
+
 
 binary.write(OUTPUT)
 output_result()
